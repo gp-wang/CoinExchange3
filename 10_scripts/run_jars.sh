d61f74c842bb41be3ae345edb959b8d50db73a66
gaopeng <gaopeng@gaopeng-CM6870>
2021-10-14 12:57:38 -0700

    foo

--- 10_scripts/run_jars.sh
+++ 10_scripts/run_jars.sh
@@ -1,16 +1,26 @@
-#!/bin/sh
+#!/bin/bash
 
-WORK_DIR="../00_framework"
+WORK_DIR=$(realpath "../00_framework")
 if [ !  -d "$WORK_DIR" ]; then
     echo "ERROR: $WORK_DIR not exists!, will exit not"
     exit 1
 fi
 
+LOG_DIR=$(realpath "../11_logs")
+if [ !  -d "$LOG_DIR" ]; then
+    echo "log dir not exist yet, creating log dir: $LOG_DIR"
+    mkdir $LOG_DIR
+fi
 
 
 apps="cloud exchange exchange-api market ucenter-api wallet chat otc-api admin bitrade-job"
 
 
 for app in $apps; do
-    xterm -title "$app" -hold -e "(cd ${WORK_DIR}/${app}/target;java -jar *.jar)" &
+    xterm -title "$app" -hold -e "(cd ${WORK_DIR}/${app}/target;java -jar *.jar 2>&1 > $LOG_DIR/$app.log & tail -f $LOG_DIR/$app.log)" &
+    if [[ "$app" == "exchange" ]]; then
+        #sleep 80
+        tail -f -n0 $LOG_DIR/$app.log |  grep -qe "程序启动成功"
+        #expect "程序启动成功"
+    fi
 done;
--- 10_scripts/start.sh
+++ 10_scripts/start.sh
@@ -1,5 +1,8 @@
+#! /bin/bash
+
 sudo systemctl start mysql
 /usr/redis/redis-server /usr/redis/redis.conf &
-/home/gaopeng/programs/zookeeper-3.4.10/bin/zkServer.sh start
 /home/gaopeng/programs/kafka_2.12-1.0.1/bin/zookeeper-server-start.sh /home/gaopeng/programs/kafka_2.12-1.0.1/config/zookeeper.properties &
 /home/gaopeng/programs/kafka_2.12-1.0.1/bin/kafka-server-start.sh /home/gaopeng/programs/kafka_2.12-1.0.1/config/server.properties &
+/home/gaopeng/programs/mongodb/mongod --config /home/gaopeng/programs/mongodb/mongodb.conf
+
